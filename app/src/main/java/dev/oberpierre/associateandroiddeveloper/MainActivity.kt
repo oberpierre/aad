0acfba0deb69d19f3289731f90ada3f9f16d9455
Pierre Obermaier <pierre.obermaier@gmail.com>
2023-05-15 21:47:47 +0800

    F Creating a simple app with text view which doubles in value upon floating button press

--- app/src/main/java/dev/oberpierre/associateandroiddeveloper/MainActivity.kt
+++ app/src/main/java/dev/oberpierre/associateandroiddeveloper/MainActivity.kt
@@ -1,20 +1,16 @@
 package dev.oberpierre.associateandroiddeveloper
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import com.google.android.material.snackbar.Snackbar
 import dev.oberpierre.associateandroiddeveloper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,12 +22,18 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            val textValue: TextView = findViewById(R.id.text_value)
+            val stringValue = textValue.text.toString()
+            val originalValue = Integer.parseInt(stringValue)
+            val newValue = MyWorker.doubleTheValue(originalValue)
+            textValue.text = newValue.toString()
+
+            Snackbar.make(
+                view,
+                "Changed value from $originalValue to $newValue.",
+                Snackbar.LENGTH_LONG
+            )
                 .setAnchorView(R.id.fab)
                 .setAction("Action", null).show()
         }
@@ -51,11 +53,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/dev/oberpierre/associateandroiddeveloper/MyWorker.kt
@@ -0,0 +1,11 @@
+package dev.oberpierre.associateandroiddeveloper
+
+class MyWorker {
+
+    companion object {
+        fun doubleTheValue(value: Int): Int {
+            return value * 2
+        }
+    }
+
+}
